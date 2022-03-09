8b7a16e3f8be90bf03da08f449febe6d1bf2da51
vinao-alt <viniss201@gmail.com>
2022-03-08 22:04:16 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,83 @@
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function adicionar() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("atividade").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Você deve escrever alguma coisa!");
+  } else {
+    document.getElementById("ul").appendChild(li);
+  }
+  document.getElementById("atividade").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+// .......................................................
+
+function toggleFAB(fab){
+	if(document.querySelector(fab).classList.contains('show')){
+  	document.querySelector(fab).classList.remove('show');
+  }else{
+  	document.querySelector(fab).classList.add('show');
+  }
+}
+
+document.querySelector('.fab .main').addEventListener('click', function(){
+	toggleFAB('.fab');
+});
+
+document.querySelectorAll('.fab ul li button').forEach((item)=>{
+	item.addEventListener('click', function(){
+		toggleFAB('.fab');
+	});
+});
+
+
+// ////////////////////////////////////////
+
+function Mycolor() {
+  var element = document.getElementById("body");
+  element.style.backgroundColor='#900';
+  }
